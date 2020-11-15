d620914d649b550349e3a1cfc5824d29c53a79ac
Kalia-Lee Rodney <kaliarodney@gmail.com>
2020-11-14 20:34:15 -0500

    Exercise 5 completed

--- world.js
+++ world.js
@@ -26,4 +26,32 @@ window.onload = function(){
         hrequest.send();
     });
 
+    var citybtn = document.getElementById("cities");
+    citybtn.addEventListener("click", function(e){
+        e.preventDefault();
+        var lookupqueryfunc = document.getElementById("country").value;
+        var hrequest = new XMLHttpRequest();
+
+        var urlcode = "world.php?country=" + lookupqueryfunc + "&context=cities";
+
+        hrequest.onreadystatechange = function() {
+            if (hrequest.readyState == XMLHttpRequest.DONE){
+                if (hrequest.status == 200) {
+                    var country = hrequest.responseText;
+                    var result = document.getElementById("result");
+                    result.innerHTML = country;
+
+                } else{
+                    alert("Error Detected");
+                }
+
+            }
+        }
+
+        hrequest.open("GET" , urlcode, true);
+        hrequest.send();
+
+    });
+
+
 }
--- world.php
+++ world.php
@@ -6,32 +6,67 @@ $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 //$stmt = $conn->query("SELECT * FROM countries");
+
+
 $country = filter_input(INPUT_GET, "country", FILTER_SANITIZE_STRING);
+$urlrequest = $_SERVER['REQUEST_URI'];
+$query = parse_url($urlrequest, PHP_URL_QUERY);
+parse_str($query, $param);
+
+
+$context = $param['context'];
+//echo $context;
+
+
+
 //echo $country ;
+$find_cities = $conn->query("SELECT * FROM countries JOIN cities ON countries.code = cities.country_code where countries.name LIKE '%$country%'");
 $find_country = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
 
 $resultcountry = $find_country->fetchAll(PDO::FETCH_ASSOC);
+$resultcities = $find_cities->fetchAll(PDO::FETCH_ASSOC);
 
 //$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
+<?php if($context == "cities"): ?>
 
 <table>
   <tr>
     <th> Name  </th>
-    <th> Continenent </th>
-    <th> Year of Independence  </th>
-    <th> Head of State </th>
+    <th> District </th>
+    <th> Population </th>
   </tr>
 
-<?php foreach ($resultcountry as $row): ?>
+<?php foreach ($resultcities as $row): ?>
 <tr>
   <td><?php echo $row['name']; ?></td>
-  <td><?php echo $row['continent']; ?></td>
-  <td><?php echo $row['independence_year']; ?></td>
-  <td><?php echo $row['head_of_state']; ?></td>
+  <td><?php echo $row['district']; ?></td>
+  <td><?php echo $row['population']; ?></td>
 </tr>
 
 <?php endforeach; ?>
+</table>
+
+<?php else: ?>
+
+  <table>
+    <tr>
+      <th> Name  </th>
+      <th> Continent </th>
+      <th> Year of Independence  </th>
+      <th> Head of State </th>
+    </tr>
+
+  <?php foreach ($resultcountry as $row): ?>
+  <tr>
+    <td><?php echo $row['name']; ?></td>
+    <td><?php echo $row['continent']; ?></td>
+    <td><?php echo $row['independence_year']; ?></td>
+    <td><?php echo $row['head_of_state']; ?></td>
+  </tr>
+
+<?php endforeach; ?>
+<?php endif ?>
   
 </table>
